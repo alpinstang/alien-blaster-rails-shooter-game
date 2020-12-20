ad77a219c5ebe2ac374afddd9d9c50eb423263a9
John McDonald <jmcdonald1991@gmail.com>
2020-12-20 00:18:10 -0600

    disable controls on death

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("General")]
+    [Tooltip("In m/s")][SerializeField] float xControlSpeed = 4f;
+    [Tooltip("In m")] [SerializeField] float xRange = 5f;
+    [Tooltip("In m/s")] [SerializeField] float yControlSpeed = 4f;
+    [Tooltip("In m")] [SerializeField] float yMin = -5f;
+    [Tooltip("In m")] [SerializeField] float yMax = 5f;
+
+    [Header("Screen-Position Based")]
+    [SerializeField] float positionPitchFactor = -2f;
+    [SerializeField] float positionYawFactor = 2f;
+
+    [Header("Control-Throw Based")]
+    [SerializeField] float controlPitchFactor = -25f;
+    [SerializeField] float controlRollFactor = -20f;
+    [SerializeField] float xThrow, yThrow;
+
+    bool isControlEnabled = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isControlEnabled)
+        {
+            ProcessTranslation();
+            ProcessRotation();
+        }
+    }
+
+    private void ProcessRotation()
+    {
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDuetoControlThrow = yThrow * controlPitchFactor;
+        float pitch = pitchDueToPosition + pitchDuetoControlThrow;
+
+        float yaw = transform.localPosition.x * positionYawFactor;
+
+        float roll = xThrow * controlRollFactor;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    private void ProcessTranslation()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * xControlSpeed * Time.deltaTime;
+        float rawNewXPos = transform.localPosition.x + xOffset;
+        float xPos = Mathf.Clamp(rawNewXPos, -xRange, xRange);
+
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffset = yThrow * yControlSpeed * Time.deltaTime;
+        float rawNewYPos = transform.localPosition.y + yOffset;
+        float yPos = Mathf.Clamp(rawNewYPos, yMin, yMax);
+
+        transform.localPosition = new Vector3(xPos, yPos, transform.localPosition.z);
+    }
+
+    void OnPlayerDeath() // called by string reference in OnCollisionHandler.cs
+    {
+        isControlEnabled = false;
+    }
+}
