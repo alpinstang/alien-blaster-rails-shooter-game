2fcb1a1e218e9d435d031ae5764bb2696bea3a73
John McDonald <jmcdonald1991@gmail.com>
2020-12-27 18:42:05 -0600

    explosions, player death

---
+++ Assets/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] GameObject deathFX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AddNonTriggerBoxCollider();
+    }
+
+    private void AddNonTriggerBoxCollider()
+    {
+        gameObject.AddComponent<BoxCollider>();
+        gameObject.GetComponent<BoxCollider>().isTrigger = false;
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        deathFX.SetActive(true);
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -2,12 +2,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] float levelLoadDelay = 1f;
+    [SerializeField] GameObject deathFX;
+
     private void OnTriggerEnter(Collider other)
     {
         StartDeathSequence();
+        deathFX.SetActive(true);
+        Invoke("ReloadScene", 5f);
     }
 
     private void StartDeathSequence()
